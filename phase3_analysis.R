ea9ab8c196ac7dc496fb0a877ddfe2e10107affc
Ross Cunning <ross.cunning@gmail.com>
2016-04-17 15:22:38 -1000

    tidy

--- phase3_analysis.R
+++ phase3_analysis.R
@@ -1,13 +1,12 @@
 # LOAD DATA AND LIBRARIES -----
-library(effects)
 library(lme4)
-library(spida)
 library(scales)
 library(lsmeans)
 library(reshape2)
 library(plyr)
 library(lattice)
 
+# Define function to use for plotting confidence intervals
 addpoly <- function(x, y1, y2, col=alpha("lightgrey", 0.8), ...){
   ii <- order(x)
   y1 <- y1[ii]
@@ -16,6 +15,7 @@ addpoly <- function(x, y1, y2, col=alpha("lightgrey", 0.8), ...){
   polygon(c(x, rev(x)), c(y1, rev(y2)), col=col, border=NA, ...)
 }
 
+# Import data
 data <- read.csv("phase3_2.csv")
 
 # Adjust and transform data -----
@@ -26,8 +26,6 @@ data$propD <- data$D.SH / (data$C.SH + data$D.SH)
 # Categorize C- and D-dominated corals based on community composition at time zero
 dom <- with(data[data$time==0, ], na.omit(data.frame(sample=sample, 
                                                      dom=ifelse(is.na(propD), NA, ifelse(propD > 0.5, "D", "C")))))
-# dom <- with(data[data$time==0, ], na.omit(data.frame(sample=sample,
-#                                                      dom=ifelse(is.na(propD), NA, ifelse(propD > 0.9, "D", ifelse(propD < 0.1, "C", NA))))))
 # Merge dominant symbiont classification with rest of data
 data <- merge(data, dom, by="sample", all.x=T)
 table(data[data$time==0, "dom"])  # 27 C-dominant and 88 D-dominant corals = 115 corals with qPCR data at t0
@@ -41,6 +39,7 @@ min(data[data$C.SH!=0,"C.SH"], na.rm=T) # detection limit for C is ~1e-6
 min(data[data$D.SH!=0,"D.SH"], na.rm=T) # detection limit for D is ~1e-4
 data[data$C.SH==0 & !is.na(data$C.SH), "C.SH"] <- 1e-6
 data[data$D.SH==0 & !is.na(data$D.SH), "D.SH"] <- 1e-4
+data$tot.SH <- data$C.SH + data$D.SH
 data[data$tot.SH<0.000101 & !is.na(data$tot.SH), "tot.SH"] <- 0.000101
 # Calculate relative change in Fv/Fm
 for (sample in data$sample) {
@@ -89,6 +88,8 @@ lmerTest::anova(mod)  # no impact of history on totSH
 plot(Effect(c("timef", "history"), mod))
 
 # Analyze Fv/Fm -----
+finalheat <- subset(data, ramp=="heat" & time==42)
+table(finalheat$dom, finalheat$fvfm)
 # Cooling treatment
 gm.cool <- gamm(rfvfm ~ dom + s(time, by=dom, k=4), random=list(mother=~1, sample=~1), 
                 data=subset(data, ramp=="cool"), na.action=na.omit)
@@ -134,36 +135,36 @@ heat.test0 <- lapply(CDdiff.95CI, function(x) x[1] < 0 & 0 < x[2])
 heat.test0[heat.test0==F]  # Shows when C and D are significantly different with p<0.05
 
 # Analyze total S/H ratio ------------
+initsh <- subset(data, time==0 & dom=="D")
+table(initsh$propD==0) # 13 all C
+table(initsh$propD==1) # 12 all D
+# 115 - 25 = 90 mixed --> 78.26% mixed
+hist(log10(initsh[initsh$dom=="C", "propD"]))
+hist(log10(1-initsh[initsh$dom=="D", "propD"]))
+boxplot(log10(1-initsh[initsh$dom=="D", "propD"]))
+
+finalsh <- subset(data, ramp=="heat" & time==42)
+table(finalsh$dom, finalsh$tot.SH)
 # Fit model for cooling treatment and test for effect of past thermal history
-coolSHmod <- lmerTest::lmer(log10(tot.SH) ~ timef * dom * history + (1|mother/sample), 
+coolSHmod <- lmerTest::lmer(log10(tot.SH) ~ timef * dom + (1|mother/sample), 
                             data=subset(data, ramp=="cool"), na.action=na.omit)
-lmerTest::anova(coolSHmod)
-coolSHlsm <- lsmeans::lsmeans(coolSHmod, specs=c("timef", "history", "dom"), by=c("dom", "timef"))
-hist.pw <- data.frame(summary(pairs(coolSHlsm))); na.omit(hist.pw)
-hist.pw[which(hist.pw$p.value < 0.01), ]  # No contrasts are significant at alpha=0.01
-coolSHmod <- update(coolSHmod, formula=~timef * dom + (1|mother/sample)) # remove history from model
 # Fit model for heating treatment and test for effect of past thermal history
-heatSHmod <- lmerTest::lmer(log10(tot.SH) ~ timef * dom * history + (1|mother/sample), 
+heatSHmod <- lmerTest::lmer(log10(tot.SH) ~ timef * dom + (1|mother/sample), 
                             data=subset(data, ramp=="heat"), na.action=na.omit)
-lmerTest::anova(heatSHmod)
-heatSHlsm <- lsmeans::lsmeans(heatSHmod, specs=c("timef", "history", "dom"), by=c("dom", "timef"))
-hist.pw <- data.frame(summary(pairs(heatSHlsm))); na.omit(hist.pw)
-hist.pw[which(hist.pw$p.value < 0.01), ]  # No contrasts are significant at alpha=0.01
-heatSHmod <- update(heatSHmod, formula=~timef * dom + (1|mother/sample)) # remove history from model
 # Compare S/H ratio within each group to time zero
 cool.dunnett <- rbind(contrast(lsmeans::lsmeans(coolSHmod, specs=c("timef", "dom")), "dunnett", by="dom"))
 heat.dunnett <- rbind(contrast(lsmeans::lsmeans(heatSHmod, specs=c("timef", "dom")), "dunnett", by="dom"))
 # Calculate percent change relative to time zero and extract significant contrasts
 cool.dunnett <- within(data.frame(summary(cool.dunnett)), {
-  loss <- paste0(round(-(1-(10^estimate)) * 100, 1), "%")
+  loss <- paste0(sprintf("%.1f", round(-(1-(10^estimate)) * 100, 1)), "%")
   day <- substr(contrast, 1, 2)
 })
-cool.diff <- cool.dunnett[cool.dunnett$p.value < 0.01, ]
+cool.diff <- cool.dunnett[cool.dunnett$p.value < 0.05, ]
 heat.dunnett <- within(data.frame(summary(heat.dunnett)), {
-  loss <- paste0(round(-(1-(10^estimate)) * 100, 1), "%")
+  loss <- paste0(sprintf("%.1f", round(-(1-(10^estimate)) * 100, 1)), "%")
   day <- substr(contrast, 1, 2)
 })
-heat.diff <- heat.dunnett[heat.dunnett$p.value < 0.01, ]
+heat.diff <- heat.dunnett[heat.dunnett$p.value < 0.05, ]
 
 # Figure 1: Fv/Fm and total S/H under (A.) cooling and (B.) heating -------------
 # Prepare raw data for plotting
@@ -183,7 +184,7 @@ par(mgp=c(1.5,0.25,0), tck=-0.06, xpd=NA)
 par(mar=c(1,3,4,1))
 plot(NA, xlim=c(0,63), ylim=c(0, 1), bty="n", tck=-0.03,
      xaxt="n", ann=F)
-title("A. Cooling", adj=0)
+title("A. Cooling", adj=0, cex.main=1.5, line=2)
 mtext(side=2, text="Relative Fv/Fm", cex=0.75, line=1.5)
 with(subset(gm.cool.pred, dom=="C"), {
   addpoly(time, uci, lci, col=alpha("blue", 0.4))
@@ -230,7 +231,7 @@ for (i in 1:nrow(cool.diff)) {
 par(mar=c(1,3,4,1))
 plot(NA, xlim=c(0,63), ylim=c(0, 1), bty="n", tck=-0.03, ylab="fvfm", xlab="days",
      xaxt="n", ann=F, xpd=NA)
-title("B. Heating", adj=0)
+title("B. Heating", adj=0, cex.main=1.5, xpd=NA, line=2)
 mtext(side=2, text="Relative Fv/Fm", cex=0.75, line=1.5)
 with(subset(gm.heat.pred, dom=="C" & time <=42), {
   addpoly(time, uci, lci, col=alpha("blue", 0.4))
@@ -240,7 +241,7 @@ with(subset(gm.heat.pred, dom=="D" & time <=42), {
   addpoly(time, uci, lci, col=alpha("red", 0.4))
   lines(time, fit)
 })
-points(c(7,14,21), c(1.11,1.08,0.99), pch="*", cex=2, xpd=T)
+points(c(7,14,21), c(1.1,1.08,0.99), pch="*", cex=2, xpd=T)
 # Plot raw data +/- standard deviation
 lapply(datlist[["heat"]], function(dom) {
   arrows(dom$time, dom$rfvfm.mean + dom$rfvfm.sd, dom$time, dom$rfvfm.mean - dom$rfvfm.sd, code=3, angle=90, length=0.05, xpd=NA,
@@ -271,66 +272,31 @@ for (i in 1:nrow(heat.diff)) {
 }
 #dev.off()
 
-# Analyze C and D abundances over time by treatment and dominant clade at start -----
-# Clade C
-df <- data[!is.na(data$tot.SH), ]
-Cmod <- lmer(log10(C.SH) ~ timef * ramp * dom + (1|mother/sample), data=df)
-Clsm <- lsmeans::lsmeans(Cmod, specs=c("timef", "ramp", "dom"))
-Clsmeans <- summary(Clsm)
-# Clade D
-Dmod <- lmer(log10(D.SH) ~ timef * ramp * dom + (1|mother/sample), data=df)
-Dlsm <- lsmeans::lsmeans(Dmod, specs=c("timef", "ramp", "dom"))
-Dlsmeans <- summary(Dlsm)
-# Combine into single data frame
-df1 <- data.frame(time=Clsmeans$timef, ramp=Clsmeans$ramp, dom=Clsmeans$dom, Cmean=Clsmeans$lsmean, Dmean=Dlsmeans$lsmean, Cse=Clsmeans$SE, Dse=Dlsmeans$SE)
-
-# Figure 2: Mixed community dynamics under (A.) cooling and (B.) heating -----
-# Plot mean abundances of C and D over time by treatment and dominant clade at start
-#pdf(file="output/Figure2.pdf", width=6.85, height=3.425)
-par(mfrow=c(1,2), mar=c(3,3,2,1), mgp=c(1.5,0.4,0), tcl=-0.3, xpd=F)
-df <- subset(df1, ramp=="cool")
-plot(NA, xlim=c(-6,0), ylim=c(-6,0), xlab="Clade D (log10 S/H)", ylab="Clade C (log10 S/H)", cex.axis=0.75, cex.lab=0.75)
-abline(a=0,b=1,lty=2)
-arrows(df$Dmean, df$Cmean+df$Cse, df$Dmean, df$Cmean-df$Cse, length=0.025, angle=90, code=3, col="black")
-arrows(df$Dmean+df$Dse, df$Cmean, df$Dmean-df$Dse, df$Cmean, length=0.025, angle=90, code=3, col="black")
-for (j in 2:nrow(df)) {
-  if (as.numeric(df$time)[j] > as.numeric(df$time)[j-1]) {
-    arrows(df$Dmean[j-1], df$Cmean[j-1], df$Dmean[j], df$Cmean[j], length=0.1, code=2, lwd=2,
-           col=c("blue","red","green")[df$dom[j]])
-  }
-}
-text(par("usr")[1], 0.5, labels=expression(bold("A. Cooling")), adj=0, xpd=NA)
-legend("bottomright", legend=c("C-dominated", "D-dominated"), 
-       lty=1, lwd=2, col=c("blue","red","green"), inset=0.05, cex=0.75, bty="n")
-df <- subset(df1, ramp=="heat")
-
-plot(NA, xlim=c(-6,0), ylim=c(-6,0), xlab="Clade D (log10 S/H)",ylab="Clade C (log10 S/H)", cex.axis=0.75, cex.lab=0.75)
-abline(a=0,b=1,lty=2)
-arrows(df$Dmean, df$Cmean+df$Cse, df$Dmean, df$Cmean-df$Cse, length=0.025, angle=90, code=3, col="black")
-arrows(df$Dmean+df$Dse, df$Cmean, df$Dmean-df$Dse, df$Cmean, length=0.025, angle=90, code=3, col="black")
-for (j in 2:nrow(df)) {
-  if (as.numeric(df$time)[j] > as.numeric(df$time)[j-1]) {
-    arrows(df$Dmean[j-1], df$Cmean[j-1], df$Dmean[j], df$Cmean[j], length=0.1, code=2, lwd=2,
-           col=c("blue","red","green")[df$dom[j]])
-  }
-}
-text(par("usr")[1], 0.5, labels=expression(bold("B. Heating")), adj=0, xpd=NA)
-#dev.off()
-
-# Modeling both C and D responses together -----
+# Modeling C and D responses together -----
 df <- data[!is.na(data$tot.SH), ]
 df2 <- melt(df, id.vars=c(1:6,9:13), value.name="SH")
 colnames(df2)[colnames(df2)=="variable"] <- "clade"
 # Fit model for cooling treatment
 coolmod <- lmerTest::lmer(log10(SH) ~ timef * dom * clade + (timef+dom+clade|mother/sample), 
                           data=subset(df2, ramp=="cool"))
-coolmod.lsm <- data.frame(summary(lsmeans(coolmod, specs=c("clade", "timef", "dom"))))
+coolmod.lsm <- lsmeans(coolmod, specs=c("clade", "timef", "dom"))
+coolmod.contr <- data.frame(summary(contrast(coolmod.lsm, "consec", by=c("dom", "clade"))))
+coolmod.sigs <- coolmod.contr[coolmod.contr$p.value < 0.05, ]
+coolmod.sigs$loss <- -(1-10^coolmod.sigs$estimate)
+coolmod.sigs
+coolmod.lsm <- data.frame(summary(coolmod.lsm))
+
 # Fit model for heating treatment
 heatmod <- lmerTest::lmer(log10(SH) ~ timef * dom * clade + (timef+dom+clade|mother/sample), 
                           data=subset(df2, ramp=="heat"))
-heatmod.lsm <- data.frame(summary(lsmeans(heatmod, specs=c("clade", "timef", "dom"))))
+heatmod.lsm <- lsmeans(heatmod, specs=c("clade", "timef", "dom"))
+heatmod.contr <- data.frame(summary(contrast(heatmod.lsm, "consec", by=c("dom", "clade"))))
+heatmod.sigs <- heatmod.contr[heatmod.contr$p.value < 0.05, ]
+heatmod.sigs$loss <- -(1-10^heatmod.sigs$estimate)
+heatmod.sigs
+heatmod.lsm <- data.frame(summary(heatmod.lsm))
 
-# Figure 2 ALT: Mixed community dynamics under (A.) cooling and (B.) heating -----
+# Figure 2: Mixed community dynamics under (A.) cooling and (B.) heating -----
 # Plot mean abundances of C and D over time by treatment and dominant clade at start
 #pdf(file="output/Figure2.pdf", width=6.85, height=3.425)
 par(mfrow=c(1,2), mar=c(3,3,2,1), mgp=c(1.5,0.4,0), tcl=-0.3, xpd=F)
@@ -348,9 +314,18 @@ for (j in 2:nrow(df)) {
            col=c("blue","red","green")[df$dom[j]])
   }
 }
+with(df[4, ], text(D.SH, C.SH, labels=expression("" %down%C), adj=c(1,-4), cex=0.7))
+with(df[8, ], text(D.SH, C.SH, labels=expression("" %down%C), adj=c(1.5,-1), cex=0.7))
+
+xx <- seq(-5,-4,len=4)
+arrows(x0=xx[-4], y0=-0.2, x1=xx[-1], code=2, length=0.07, lwd=1.5, col="blue")
+arrows(x0=xx[-4], y0=-0.4, x1=xx[-1], code=2, length=0.07, lwd=1.5, col="red")
+text(xx, -0.6, labels=c(24,20,18,15), cex=0.7, adj=c(0.5, 0.35))
+text(-5.5, c(-0.2, -0.4, -0.6), labels=c("C-dom.", "D-dom.", "°C"), cex=0.75, adj=c(0.5, 0.35))
+rect(-6,-0.75,-3.8,0)
+
 text(par("usr")[1], 0.5, labels=expression(bold("A. Cooling")), adj=0, xpd=NA)
-legend("bottomright", legend=c("C-dominated", "D-dominated"), 
-       lty=1, lwd=2, col=c("blue","red","green"), inset=0.05, cex=0.75, bty="n")
+
 
 df <- dcast(heatmod.lsm, timef + dom ~ clade, value.var="lsmean")
 df$C.se <- dcast(heatmod.lsm, timef + dom ~ clade, value.var="SE")$C.SH
@@ -367,4 +342,15 @@ for (j in 2:nrow(df)) {
   }
 }
 text(par("usr")[1], 0.5, labels=expression(bold("B. Heating")), adj=0, xpd=NA)
+with(df[2, ], text(D.SH, C.SH, labels=expression("" %down%C), adj=c(1.2,-12), cex=0.7))
+with(df[3, ], text(D.SH, C.SH, labels=expression("" %down%C), adj=c(0.55,-4), cex=0.7))
+with(df[5, ], text(D.SH, C.SH, labels=expression("" %down%C), adj=c(0.9,-4.5), cex=0.7))
+with(df[6, ], text(D.SH, C.SH, labels=expression("" %down%D), adj=c(-1,-1.4), cex=0.7))
+
+xx <- seq(-5,-4,len=3)
+arrows(x0=xx[-3], y0=-0.2, x1=xx[-1], code=2, length=0.07, lwd=1.5, col="blue")
+arrows(x0=xx[-3], y0=-0.4, x1=xx[-1], code=2, length=0.07, lwd=1.5, col="red")
+text(xx, -0.6, labels=c(29,33,35), cex=0.7, adj=c(0.5, 0.35))
+text(-5.5, c(-0.2, -0.4, -0.6), labels=c("C-dom.", "D-dom.", "°C"), cex=0.75, adj=c(0.5, 0.35))
+rect(-6,-0.75,-3.8,0)
 #dev.off()
